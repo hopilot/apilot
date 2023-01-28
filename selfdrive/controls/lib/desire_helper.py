e9305ed8fc7a41a51874bb8981b5d8734a6c32fd
Chanho Jung <jungch@naver.com>
2023-01-28 19:58:01 +0900

    roadEdges
    2M roadEdges...
    
    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -1,16 +1,14 @@
 from cereal import log
 from common.conversions import Conversions as CV
 from common.realtime import DT_MDL
-from common.conversions import Conversions as CV
-from common.params import Params
+from common.numpy_fast import interp
 
 AUTO_LCA_START_TIME = 0.2
 
 LaneChangeState = log.LateralPlan.LaneChangeState
 LaneChangeDirection = log.LateralPlan.LaneChangeDirection
 
-#LANE_CHANGE_SPEED_MIN = 15 * CV.MPH_TO_MS
-LANE_CHANGE_SPEED_MIN = 30 * CV.KPH_TO_MS
+LANE_CHANGE_SPEED_MIN = 30 * CV.KPH_TO_MS # 20 * CV.MPH_TO_MS
 LANE_CHANGE_TIME_MAX = 10.
 
 DESIRES = {
@@ -55,8 +53,8 @@ class DesireHelper:
     one_blinker = carstate.leftBlinker != carstate.rightBlinker
     below_lane_change_speed = v_ego < LANE_CHANGE_SPEED_MIN
 
-    left_road_edge = -md.roadEdges[0].y[0]
-    right_road_edge = md.roadEdges[1].y[0]
+    left_road_edge = -interp(2.0, md.roadEdges[0].x, md.roadEdges[0].y)
+    right_road_edge = interp(2.0, md.roadEdges[1].x, md.roadEdges[1].y)
 
     if (not lateral_active) or (self.lane_change_timer > LANE_CHANGE_TIME_MAX) or (not one_blinker) or (not self.lane_change_enabled):
       self.lane_change_state = LaneChangeState.off
