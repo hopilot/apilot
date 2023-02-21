3f986cd441ce27dc22163bdc534b2af14ec17abd
Chanho Jung <jungch@naver.com>
2023-02-21 20:47:20 +0900

    Revert "dmoji icon"
    This reverts commit e172d935a68ea3f1ab6bf84ed9adc94ae992b26b.

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -7,8 +7,6 @@
 #include "selfdrive/ui/qt/widgets/cameraview.h"
 #include "selfdrive/ui/ui.h"
 
-const int btn_size = 192;
-const int img_size = (btn_size / 4) * 3;
 #include <QTimer>
 #include <QMap>
 #include "selfdrive/ui/qt/screenrecorder/screenrecorder.h"
@@ -29,21 +27,6 @@ protected:
 private:
   QColor bg;
   Alert alert = {};
-};
-
-class ExperimentalButton : public QPushButton {
-  Q_OBJECT
-
-public:
-  explicit ExperimentalButton(QWidget *parent = 0);
-  void updateState(const UIState &s);
-
-private:
-  void paintEvent(QPaintEvent *event) override;
-
-  Params params;
-  QPixmap engage_img;
-  QPixmap experimental_img;
 };
 
 // container window for the NVG UI
@@ -74,12 +57,6 @@ protected:
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
   inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
-
-  //ExperimentalButton *experimental_btn;
-  bool dmActive = false;
-  bool hideDM = false;
-  QPixmap dm_img;
-  float dm_fade_state = 1.0;
 
   double prev_draw_t = 0;
   FirstOrderFilter fps_filter;
@@ -127,7 +104,6 @@ protected:
   void drawTurnSignals(QPainter &p);
   void drawGpsStatus(QPainter &p);
   void drawDebugText(QPainter &p);
-  void drawDriverState(QPainter &painter, const UIState *s);
   void drawHud(QPainter &p, const cereal::ModelDataV2::Reader &model);
 
   int m_fps = 0;
