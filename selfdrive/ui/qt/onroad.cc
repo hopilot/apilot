8924b6020814e44088be9a5e5313bed44fdb0325
hopilot <119085851+hopilot@users.noreply.github.com>
2023-03-27 17:32:08 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -521,7 +521,7 @@ void AnnotatedCameraWidget::drawLead(QPainter &painter, const cereal::RadarState
   // float g_yo = sz / 10;
 
   // QPointF glow[] = {{x + (sz * 1.35) + g_xo, y + sz + g_yo}, {x, y - g_yo}, {x - (sz * 1.35) - g_xo, y + sz + g_yo}};
-  QPointF glow[] = {{x + (sz * 1.5), y + 1.5 * sz}, {x + sz, y + sz}, {x - sz, y + sz}, {x - (sz * 1.5), y + 1.5 * sz}};
+  QPointF glow[] = {{x + (sz * 1.5), y + 1.5 * sz}, {x + (sz * 1.25), y + sz}, {x - (sz * 1.25), y + sz}, {x - (sz * 1.5), y + 1.5 * sz}};
   painter.setBrush(is_radar ? QColor(86, 121, 216, 255) : QColor(218, 202, 37, 255));
   painter.drawPolygon(glow, std::size(glow));
 
