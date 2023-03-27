bb43613cb6ee321151c8d22a2cd2af7b94c59c42
chanhojung <jungch@naver.com>
2023-03-27 11:03:02 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -530,9 +530,10 @@ void AnnotatedCameraWidget::drawLead(QPainter &painter, const cereal::RadarState
   // painter.setBrush(redColor(fillAlpha));
   // painter.drawPolygon(chevron, std::size(chevron));
   // square
-  QPointF square[] = {{x - (sz * 1.25)/2, y}, {x + (sz * 1.25)/2, y}, {x + (sz * 1.25)/2, y + sz}, {x - (sz * 1.25)/2, y + sz}};
+  //QPointF square[] = {{x - (sz * 1.25)/2, y}, {x + (sz * 1.25)/2, y}, {x + (sz * 1.25)/2, y + sz}, {x - (sz * 1.25)/2, y + sz}};
   painter.setBrush(redColor(fillAlpha));
-  painter.drawPolygon(square, std::size(square));
+  //painter.drawPolygon(square, std::size(square));
+  painter.drawRoundedRect(QRectF(x - (sz * 1.25)/2, y, sz * 1.25, sz), sz/4, sz/4); //test
 
   painter.restore();
 }
