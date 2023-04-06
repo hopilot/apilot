7218640b0193c373cf5398f6ed5767ef2e50737a
Chanho Jung <jungch@naver.com>
2023-04-06 23:24:19 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -224,15 +224,15 @@ class CarController:
               elif CS.out.cruiseGap != hud_control.cruiseGap:
                 can_sends.append(hyundaican.create_clu11_button(self.packer, self.frame, CS.clu11, Buttons.GAP_DIST, self.CP.carFingerprint))
                 #print("currentGap = {}, target = {}".format(CS.out.cruiseGap, hud_control.cruiseGap))
+            elif CS.out.cruiseGap != hud_control.cruiseGap:
+              can_sends.append(hyundaican.create_clu11_button(self.packer, self.frame, CS.clu11, Buttons.GAP_DIST, self.CP.carFingerprint))
+              #print("currentGap = {}, target = {}".format(CS.out.cruiseGap, hud_control.cruiseGap))
             elif target < current and current>= 31:
               can_sends.append(hyundaican.create_clu11_button(self.packer, self.frame, CS.clu11, Buttons.SET_DECEL, self.CP.carFingerprint))
               #CC.debugTextCC = "BTN:--,T:{:.1f},C:{:.1f}".format(target, current)
             elif target > current and current < 160:
               can_sends.append(hyundaican.create_clu11_button(self.packer, self.frame, CS.clu11, Buttons.RES_ACCEL, self.CP.carFingerprint))
               #CC.debugTextCC = "BTN:++,T:{:.1f},C:{:.1f}".format(target, current)
-            elif CS.out.cruiseGap != hud_control.cruiseGap:
-              can_sends.append(hyundaican.create_clu11_button(self.packer, self.frame, CS.clu11, Buttons.GAP_DIST, self.CP.carFingerprint))
-              print("currentGap = {}, target = {}".format(CS.out.cruiseGap, hud_control.cruiseGap))
 
       CC.debugTextCC = "230218a"
       if self.CP.carFingerprint in (CAR.GENESIS_G90_2019, CAR.GENESIS_G90, CAR.K7):
